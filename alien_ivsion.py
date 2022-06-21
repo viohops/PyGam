4c259cf5ad76fd8058abfed361959fc087a1d19f
scott <vitalii.programm@gmail.com>
2022-06-21 22:01:46 +0700

    ref

--- alien_ivsion.py
+++ alien_ivsion.py
@@ -19,16 +19,23 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
-            #Отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            #Отображениепоследнего прорисованого экрана
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             
             
+    def _check_events(self):
+        """Обробатывает нажатие клавишь и события мыши"""
+        #Отслеживание событий клавиатуры и мыши
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def _update_screen(self):
+        #При каждом проходе цикла перерисовывается экран
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #Отображениепоследнего прорисованого экрана
+        pygame.display.flip()
               
 if __name__ == '__main__':
     #Создание экземпляра и запуск игры
